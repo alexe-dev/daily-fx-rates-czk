5fd2ba490b881c9ee588161d3e901af90642dc1d
Alex Alexeev <work2alex@gmail.com>
2023-11-16 19:16:42 +0100

    feat: add styled components and outline the structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
+import styled from 'styled-components';
 import './App.css';
 
+const Title = styled.h1`
+  font-size: 2em;
+  text-align: center;
+  color: black;
+`;
+
 function App() {
+  // 1. fetch data from API https://www.cnb.cz/en/financial-markets/foreign-exchange-market/central-bank-exchange-rate-fixing/central-bank-exchange-rate-fixing/daily.txt
+  // 2. process data
+  // 3. create table or stacked list component to display data, render it
+  // 4. create form component to calculate amount
+  // 5. add logic for calculating amount on form submit (button or real time)
+  // 5.1 input czk amount and select currency
+  // 5.2 calculate amount in selected currency
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <Title>CNB FX Daily Rates</Title>
       </header>
+      <main>
+        <section>{/* section for displaying Rates  */}</section>
+        <section>{/* section for form to calculate amount */}</section>
+      </main>
     </div>
   );
 }
