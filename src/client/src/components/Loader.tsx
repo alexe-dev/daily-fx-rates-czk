a38bde6a051322ec1894c3fce1e0ef599045f7b2
Alex Alexeev <work2alex@gmail.com>
2023-11-19 20:12:14 +0100

    fix: adjust loader styles

--- src/client/src/components/Loader.tsx
+++ src/client/src/components/Loader.tsx
@@ -6,12 +6,7 @@ export const Loader = styled.div`
   height: 48px;
   border: 10px solid transparent;
   border-radius: 50%;
-  background-image: linear-gradient(
-      8deg,
-      rgba(134, 66, 201, 0.9),
-      rgba(17, 87, 115, 0.92) 42.02%,
-      rgba(248, 103, 103, 0.97)
-    ),
+  background-image: linear-gradient(8deg, rgba(134, 66, 201, 0.9), rgb(69 42 101) 42.02%, rgba(248, 103, 103, 0.97)),
     linear-gradient(282deg, #ffd5a9, #ff7fd1);
   background-origin: border-box;
   background-clip: content-box, border-box;
